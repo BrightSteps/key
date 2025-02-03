5da2f50eb4b56a503fbec77c143c73a56e5474ed
Hajdi Klincharska <hajdi.kostova@gmail.com>
2025-02-03 15:14:24 +0100

    Add script to log supported browsers

---
+++ log-supported-browsers.mjs
@@ -0,0 +1,5 @@
+import browserslist from 'browserslist';
+const targets = await import('./config/targets.js');
+
+const browsers = browserslist(targets.browsers);
+console.log('Supported Browsers:', browsers);
